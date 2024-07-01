35ec061da28062befe647bc30e2beae8236d38f3
ねこがくれ <156896971+nekogakure@users.noreply.github.com>
2024-07-01 16:29:04 +0900

    Update asdf.js

--- asdf.js
+++ asdf.js
@@ -1,1 +1,1 @@
-javascript:import{ link }from'./scratch-comment-emulator-nekogakure.html to scratch-comment-emulator-nekogakure.js';link();
+javascript:import{ link }from'https://raw.githubusercontent.com/nekogakure/Discussion/main/scratch-comment-emulator-nekogakure.js';link();
